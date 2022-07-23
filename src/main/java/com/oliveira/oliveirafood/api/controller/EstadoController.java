a3acf8ff2c50fa77009f30f7f7fc8561c0e34e99
alissonos <alissonoliveirasilva.oliveira@gmail.com>
2022-07-23 12:03:10 -0300

    criação Estado Controller

--- src/main/java/com/oliveira/oliveirafood/api/controller/EstadoController.java
+++ src/main/java/com/oliveira/oliveirafood/api/controller/EstadoController.java
@@ -2,6 +2,8 @@ package com.oliveira.oliveirafood.api.controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,8 +14,10 @@ import com.oliveira.oliveirafood.domain.repository.EstadoRepository;
 @RequestMapping("/estados")
 public class EstadoController {
 	
+	@Autowired
 	private EstadoRepository estadoRepository;
 	
+	@GetMapping
 	public List<Estado> listar() {
 		return estadoRepository.listar();
 	}
