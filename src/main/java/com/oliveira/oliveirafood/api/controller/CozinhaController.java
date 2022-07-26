1572193524044a3a0e05d9e33130106506e84c51
alissonos <alissonoliveirasilva.oliveira@gmail.com>
2022-07-26 14:09:33 -0300

    Método POST Cozinha e customização do HttpStatus.

--- src/main/java/com/oliveira/oliveirafood/api/controller/CozinhaController.java
+++ src/main/java/com/oliveira/oliveirafood/api/controller/CozinhaController.java
@@ -9,7 +9,10 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.oliveira.oliveirafood.api.model.CozinhasXmlWrapper;
@@ -45,6 +48,11 @@ public class CozinhaController {
 		 return ResponseEntity.notFound().build();
 	}
 	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Cozinha adicionar (@RequestBody Cozinha cozinha) {
+		return cozinhaRepository.salvar(cozinha);
+	}
 }
 
 
