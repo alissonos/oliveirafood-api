1e8d703f0a5d1d929c6e100b80e99dfaa1fa5428
alissonos <alissonoliveirasilva.oliveira@gmail.com>
2022-08-08 11:30:34 -0300

    Implementando um repositório SDJ customizado

--- src/main/java/com/oliveira/oliveirafood/domain/repository/RestauranteRepository.java
+++ src/main/java/com/oliveira/oliveirafood/domain/repository/RestauranteRepository.java
@@ -2,14 +2,25 @@ package com.oliveira.oliveirafood.domain.repository;
 
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.oliveira.oliveirafood.domain.model.Restaurante;
 
-public interface RestauranteRepository extends JpaRepository<Restaurante, Long> {
+public interface RestauranteRepository extends JpaRepository<Restaurante, Long>, RestauranteRepositoryQueries {
 		
-	List<Restaurante> findByTaxaFreteBetween(BigDecimal taxaInicial, BigDecimal taxaFinal);
+	List<Restaurante> queryByTaxaFreteBetween(BigDecimal taxaInicial, BigDecimal taxaFinal);
 	
-	List<Restaurante> findByNomeContainingAndCozinhaId(String nome, Long cozinha);
+//	@Query("from Restaurante where nome like %:nome% and cozinha.id = :id")
+	List<Restaurante> consultarPorNome(String nome,@Param("id") Long cozinha);
+	
+//	List<Restaurante> findByNomeContainingAndCozinhaId(String nome, Long cozinha);
+	
+	Optional<Restaurante> findFirstRestauranteByNomeContaining(String nome);
+	
+	List<Restaurante> findTop2ByNomeContaining(String nome);
+	
+	int countByCozinhaId(Long cozinha);
 }
---
+++ src/main/java/com/oliveira/oliveirafood/domain/repository/RestauranteRepositoryQueries.java
@@ -0,0 +1,12 @@
+package com.oliveira.oliveirafood.domain.repository;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import com.oliveira.oliveirafood.domain.model.Restaurante;
+
+public interface RestauranteRepositoryQueries {
+
+	List<Restaurante> find(String nome, BigDecimal taxaFreteInicial, BigDecimal taxaFreteFinal);
+
+}
