8736cb447f90b1a75eb8559a4879a1c00c35c134
alissonos <alissonoliveirasilva.oliveira@gmail.com>
2022-08-23 14:21:48 -0300

    Mapeando classes incorporáveis com @Embedded e @Embeddable

--- src/main/java/com/oliveira/oliveirafood/domain/model/Restaurante.java
+++ src/main/java/com/oliveira/oliveirafood/domain/model/Restaurante.java
@@ -7,6 +7,7 @@ import java.util.List;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -37,6 +38,9 @@ public class Restaurante {
 	@ManyToOne
 	@JoinColumn(name = "cozinha_id", nullable = false)
 	private Cozinha cozinha;
+	
+	@Embedded
+	private Endereco endereco;
 	
 	@JsonIgnore
 	@ManyToMany
