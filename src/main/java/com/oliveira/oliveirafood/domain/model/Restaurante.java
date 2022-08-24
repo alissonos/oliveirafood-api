f98e87905cf3b601bc33cb47ceea3d7ead2b3d51
alissonos <alissonoliveirasilva.oliveira@gmail.com>
2022-08-24 19:37:02 -0300

    Entidade Permissao e seus relacionamentos.

--- src/main/java/com/oliveira/oliveirafood/domain/model/Restaurante.java
+++ src/main/java/com/oliveira/oliveirafood/domain/model/Restaurante.java
@@ -20,6 +20,7 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
@@ -64,6 +65,9 @@ public class Restaurante {
 		inverseJoinColumns = @JoinColumn(name = "forma_pagamento_id"))
 	private List<FormaPagamento> formasPagamento = new ArrayList<>();
 	
+	@JsonIgnore
+	@OneToMany(mappedBy = "restaurante")
+	private List<Produto> produtos = new ArrayList<>();   
 }
 
 
