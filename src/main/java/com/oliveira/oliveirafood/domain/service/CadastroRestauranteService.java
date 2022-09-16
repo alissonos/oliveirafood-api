045d8f509b1c4b774ba3b1aceeeb1e84d7226d4b
alissonos <alissonoliveirasilva.oliveira@gmail.com>
2022-09-16 19:13:35 -0300

    Refatoração da Entidade Restaurante.

--- src/main/java/com/oliveira/oliveirafood/domain/service/CadastroRestauranteService.java
+++ src/main/java/com/oliveira/oliveirafood/domain/service/CadastroRestauranteService.java
@@ -6,28 +6,33 @@ import org.springframework.stereotype.Service;
 import com.oliveira.oliveirafood.domain.exception.EntidadeNaoEncontradaException;
 import com.oliveira.oliveirafood.domain.model.Cozinha;
 import com.oliveira.oliveirafood.domain.model.Restaurante;
-import com.oliveira.oliveirafood.domain.repository.CozinhaRepository;
 import com.oliveira.oliveirafood.domain.repository.RestauranteRepository;
 
 @Service
 public class CadastroRestauranteService {
 
+	private static final String MSG_RESTAURANTE_NAO_ENCONTRADO = "Não existe um cadastro de restaurante com código %d";
+
 	@Autowired
 	private RestauranteRepository restauranteRepository;
 	
 	@Autowired
-	private CozinhaRepository cozinhaRepository;
+	private CadastroCozinhaService cadastroCozinhaService;
 	
 	public Restaurante salvar(Restaurante restaurante) {
-		Long cozinhaId = restaurante.getCozinha().getId();
-		
-		Cozinha cozinha = cozinhaRepository.findById(cozinhaId)
-			.orElseThrow(() -> new EntidadeNaoEncontradaException(
-					String.format("Não existe cadastro de cozinha com código %d", cozinhaId)));
-		
-		restaurante.setCozinha(cozinha);
-		
-		return restauranteRepository.save(restaurante);
+	    Long cozinhaId = restaurante.getCozinha().getId();
+	    
+	    Cozinha cozinha = cadastroCozinhaService.buscarOuFalhar(cozinhaId);
+	    
+	    restaurante.setCozinha(cozinha);
+	    
+	    return restauranteRepository.save(restaurante);
+	}
+	
+	public Restaurante buscarOuFalhar(Long restauranteId) {
+	    return restauranteRepository.findById(restauranteId)
+	        .orElseThrow(() -> new EntidadeNaoEncontradaException(
+	                String.format(MSG_RESTAURANTE_NAO_ENCONTRADO, restauranteId)));
 	}
 	
 }
