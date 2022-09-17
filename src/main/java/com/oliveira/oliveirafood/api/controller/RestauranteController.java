36f9cdab7f86aea57ba15ee99a0d53e9429dec18
alissonos <alissonoliveirasilva.oliveira@gmail.com>
2022-09-17 19:54:16 -0300

    Refatoração

--- src/main/java/com/oliveira/oliveirafood/api/controller/RestauranteController.java
+++ src/main/java/com/oliveira/oliveirafood/api/controller/RestauranteController.java
@@ -7,7 +7,6 @@ import java.util.Map;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.util.ReflectionUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -20,7 +19,6 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.oliveira.oliveirafood.domain.exception.EntidadeNaoEncontradaException;
 import com.oliveira.oliveirafood.domain.model.Restaurante;
 import com.oliveira.oliveirafood.domain.repository.RestauranteRepository;
 import com.oliveira.oliveirafood.domain.service.CadastroRestauranteService;
--- src/main/java/com/oliveira/oliveirafood/domain/service/CadastroCidadeService.java
+++ src/main/java/com/oliveira/oliveirafood/domain/service/CadastroCidadeService.java
@@ -8,15 +8,29 @@ import org.springframework.stereotype.Service;
 import com.oliveira.oliveirafood.domain.exception.EntidadeEmUsoException;
 import com.oliveira.oliveirafood.domain.exception.EntidadeNaoEncontradaException;
 import com.oliveira.oliveirafood.domain.model.Cidade;
+import com.oliveira.oliveirafood.domain.model.Estado;
 import com.oliveira.oliveirafood.domain.repository.CidadeRepository;
 
 @Service
 public class CadastroCidadeService {
 
+	private static final String MSG_CIDADE_EM_USO = "Cidade de código %d não pode ser removida, pois está em uso";
+	
+	private static final String MSG_CIDADE_NAO_ENCONTRADA = "Não existe um cadastro de cidade com código %d";
+	
 	@Autowired
 	private CidadeRepository cidadeRepository;
 	
+	@Autowired
+	private CadastroEstadoService cadastroEstado;
+	
 	public Cidade salvar(Cidade cidade) {
+		Long estadoId = cidade.getEstado().getId();
+		
+		Estado estado = cadastroEstado.buscarOuFalhar(estadoId);
+		
+		cidade.setEstado(estado);
+		
 		return cidadeRepository.save(cidade);
 	}
 	
@@ -26,18 +40,18 @@ public class CadastroCidadeService {
 			
 		} catch (EmptyResultDataAccessException e) {
 			throw new EntidadeNaoEncontradaException(
-				String.format("Não existe um cadastro de cidade com código %d", cidadeId));
+				String.format(MSG_CIDADE_NAO_ENCONTRADA, cidadeId));
 		
 		} catch (DataIntegrityViolationException e) {
 			throw new EntidadeEmUsoException(
-				String.format("Cidade de código %d não pode ser removida, pois está em uso", cidadeId));
+				String.format(MSG_CIDADE_EM_USO, cidadeId));
 		}
 	}
 
 	public Cidade buscarOuFalhar(Long cidadeId) {
 		return cidadeRepository.findById(cidadeId)
 				.orElseThrow(() -> new EntidadeNaoEncontradaException(
-						String.format("Não existe um cadastro de cidade com código %d", cidadeId)));
+						String.format(MSG_CIDADE_NAO_ENCONTRADA, cidadeId)));
 	}
 }
 
