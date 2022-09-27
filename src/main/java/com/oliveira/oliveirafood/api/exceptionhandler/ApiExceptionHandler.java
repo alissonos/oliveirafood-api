ed51ad693cf7570c5d8314b8ff5ea43b83da3f40
alissonos <alissonoliveirasilva.oliveira@gmail.com>
2022-09-27 10:26:49 -0300

    Customizando o corpo da resposta padrão de ResponseEntityExceptionHandler

--- src/main/java/com/oliveira/oliveirafood/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/oliveira/oliveirafood/api/exceptionhandler/ApiExceptionHandler.java
@@ -1,11 +1,13 @@
 package com.oliveira.oliveirafood.api.exceptionhandler;
 
+import java.net.http.HttpHeaders;
 import java.time.LocalDateTime;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import com.oliveira.oliveirafood.domain.exception.EntidadeEmUsoException;
@@ -40,5 +42,24 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 				.mensagem(e.getMessage()).build();
 		
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(problema);
+	}
+	
+	@Override
+	protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, HttpHeaders headers,
+			HttpStatus status, WebRequest request) {
+		
+		if (body == null) {
+			body = Problema.builder()
+					.dataHora(LocalDateTime.now())
+					.mensagem(status.getReasonPhrase())
+					.build();
+		} else if (body instanceof String) {
+			body = Problema.builder()
+					.dataHora(LocalDateTime.now())
+					.mensagem((String) body)
+					.build();
+		}
+		
+		return super.handleExceptionInternal(ex, body, headers, status, request);
 	}
 }
