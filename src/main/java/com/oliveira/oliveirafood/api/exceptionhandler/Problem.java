46677cdd780679a251ce1a0db36991b116e2b273
alissonos <alissonoliveirasilva.oliveira@gmail.com>
2022-10-26 12:08:10 -0300

    Estendendo o Problem Details para adicionar as propriedades com constraints violadas

--- src/main/java/com/oliveira/oliveirafood/api/exceptionhandler/Problem.java
+++ src/main/java/com/oliveira/oliveirafood/api/exceptionhandler/Problem.java
@@ -1,6 +1,7 @@
 package com.oliveira.oliveirafood.api.exceptionhandler;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
@@ -12,13 +13,20 @@ import lombok.Getter;
 @Getter
 @Builder
 public class Problem {
-	
+
 	private Integer status;
+	private LocalDateTime timestamp;
 	private String type;
 	private String title;
 	private String detail;
-	
 	private String userMessage;
-	
-	private LocalDateTime timestamp;
+	private List<Field> fields;
+
+	@Getter
+	@Builder
+	public static class Field {
+		private String name;
+		private String userMessage;
+	}
+
 }
