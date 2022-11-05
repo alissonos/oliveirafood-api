31652c957f4e621948fce3483700662294e18ea5
alissonos <alissonoliveirasilva.oliveira@gmail.com>
2022-11-05 12:21:24 -0300

    Agrupando e restringindo constraints que devem ser usadas na validação

---
+++ src/main/java/com/oliveira/oliveirafood/Groups.java
@@ -0,0 +1,8 @@
+package com.oliveira.oliveirafood;
+
+public interface Groups {
+	
+	public interface CadastroRestaurante {
+		
+	}
+}
--- src/main/java/com/oliveira/oliveirafood/domain/model/Cozinha.java
+++ src/main/java/com/oliveira/oliveirafood/domain/model/Cozinha.java
@@ -8,9 +8,12 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonRootName;
+import com.oliveira.oliveirafood.Groups;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -20,12 +23,14 @@ import lombok.EqualsAndHashCode;
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
 public class Cozinha {
-
+	
+	@NotNull(groups = Groups.CadastroRestaurante.class)
 	@EqualsAndHashCode.Include
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
+	@NotBlank
 	@Column(nullable = false)
 	private String nome;
 	
