bde74e9595ef8dcdfbc537790836a47be4e7b90a
alissonos <alissonoliveirasilva.oliveira@gmail.com>
2022-12-05 23:08:03 -0300

    Preparando o projeto para testes de integração

--- src/test/java/com/oliveira/oliveirafood/OliveirafoodApiApplicationTests.java
+++ src/test/java/com/oliveira/oliveirafood/OliveirafoodApiApplicationTests.java
@@ -1,13 +1,20 @@
 package com.oliveira.oliveirafood;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.Assert.assertFalse;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class OliveirafoodApiApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertFalse(false);
 	}
 
 }
