57d7aec33ebd942e0faf40ff314cd54777d441e6
alissonos <alissonoliveirasilva.oliveira@gmail.com>
2022-12-06 23:23:47 -0300

    Criando e rodando um teste de integração com Spring Boot, JUnit e AssertJ

---
+++ src/test/java/com/oliveira/oliveirafood/CadastroCozinhaIntegrationTests.java
@@ -0,0 +1,54 @@
+package com.oliveira.oliveirafood;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import javax.validation.ConstraintViolationException;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.oliveira.oliveirafood.domain.model.Cozinha;
+import com.oliveira.oliveirafood.domain.service.CadastroCozinhaService;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CadastroCozinhaIntegrationTests {
+
+	@Autowired
+	private CadastroCozinhaService cadastroCozinha;
+	
+	@Test
+	public void testarCadastroCozinhaComSucesso() {
+		Cozinha novaCozinha = new Cozinha();
+		novaCozinha.setNome("Chinesa");
+		
+		novaCozinha = cadastroCozinha.salvar(novaCozinha);
+		
+		assertThat(novaCozinha).isNotNull();
+		assertThat(novaCozinha.getId()).isNotNull();
+	}
+	
+	@Test(expected = ConstraintViolationException.class)
+	public void testarCadastroCozinhaSemNome() {
+		Cozinha novaCozinha = new Cozinha();
+		novaCozinha.setNome(null);
+		
+		novaCozinha = cadastroCozinha.salvar(novaCozinha);
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
