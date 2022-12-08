4f9e1128b98779c5b5854764a9af7ac034f6635c
alissonos <alissonoliveirasilva.oliveira@gmail.com>
2022-12-08 11:05:02 -0300

    Configurando Maven Failsafe Plugin no projeto

--- src/test/java/com/oliveira/oliveirafood/CadastroCozinhaIT.java
+++ src/test/java/com/oliveira/oliveirafood/CadastroCozinhaIT.java
@@ -1,12 +1,10 @@
 package com.oliveira.oliveirafood;
 
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 import javax.validation.ConstraintViolationException;
 
 import org.junit.Test;
-import org.junit.jupiter.api.Assertions;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -19,59 +17,37 @@ import com.oliveira.oliveirafood.domain.service.CadastroCozinhaService;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class CadastroCozinhaIntegrationTests {
+public class CadastroCozinhaIT {
 
 	@Autowired
 	private CadastroCozinhaService cadastroCozinha;
-	
+
 	@Test
 	public void deveAtribuirId_QuandoCadastrarCozinhaComDadosCorretos() {
 		Cozinha novaCozinha = new Cozinha();
 		novaCozinha.setNome("Chinesa");
-		
+
 		novaCozinha = cadastroCozinha.salvar(novaCozinha);
-		
+
 		assertThat(novaCozinha).isNotNull();
 		assertThat(novaCozinha.getId()).isNotNull();
 	}
-	
+
 	@Test(expected = ConstraintViolationException.class)
-	public void deveFalhar_QuandoCadastrarCozinhaSemNome()  {
+	public void deveFalhar_QuandoCadastrarCozinhaSemNome() {
 		Cozinha novaCozinha = new Cozinha();
 		novaCozinha.setNome(null);
-		
+
 		novaCozinha = cadastroCozinha.salvar(novaCozinha);
 	}
-	
-	@Test
+
+	@Test(expected = EntidadeEmUsoException.class)
 	public void deveFalhar_QuandoExcluirCozinhaEmUso() {
-		EntidadeEmUsoException erroEsperado = Assertions.assertThrows(EntidadeEmUsoException.class, () -> {
-			cadastroCozinha.excluir(1L);
-		});
-		assertThat(erroEsperado).isNotNull();	
+		cadastroCozinha.excluir(1L);
 	}
-	
-	@Test
+
+	@Test(expected = CozinhaNaoEncontradaException.class)
 	public void deveFalhar_QuandoExcluirCozinhaInexistente() {
-
-		CozinhaNaoEncontradaException erroEsperado =
-				Assertions.assertThrows(CozinhaNaoEncontradaException.class, () -> {
-					cadastroCozinha.excluir(100L);
-				});
-
-		assertThat(erroEsperado).isNotNull();
-
+		cadastroCozinha.excluir(100L);
 	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
