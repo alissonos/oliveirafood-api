80ed36db56b0541d336a2caa0d047f73e88cff1d
alissonos <alissonoliveirasilva.oliveira@gmail.com>
2022-12-08 22:57:24 -0300

    Validando o corpo da resposta HTTP

--- src/test/java/com/oliveira/oliveirafood/CadastroCozinhaIT.java
+++ src/test/java/com/oliveira/oliveirafood/CadastroCozinhaIT.java
@@ -1,6 +1,8 @@
 package com.oliveira.oliveirafood;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.hasItems;
+import static org.hamcrest.Matchers.hasSize;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -31,5 +33,19 @@ public class CadastroCozinhaIT {
 			.get()
 		.then()
 			.statusCode(HttpStatus.OK.value());
+	}
+	@Test
+	public void deveConter4Cozinhas_QuandoConsultarCozinhas() {
+		RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+		
+		given()
+			.basePath("/cozinhas")
+			.port(port)
+			.accept(ContentType.JSON)
+		.when()
+			.get()
+		.then()
+			.body("", hasSize(4))
+			.body("nome", hasItems("Indiana", "Tailandesa"));
 	}
 }
