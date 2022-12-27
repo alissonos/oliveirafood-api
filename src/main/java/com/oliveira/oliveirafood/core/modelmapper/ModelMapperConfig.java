bc4b0938e467e88c0371aa7d59275ef9125bf47d
alissonos <alissonoliveirasilva.oliveira@gmail.com>
2022-12-26 22:11:46 -0300

    Customizando o mapeamento de propriedades com ModelMapper

--- src/main/java/com/oliveira/oliveirafood/core/modelmapper/ModelMapperConfig.java
+++ src/main/java/com/oliveira/oliveirafood/core/modelmapper/ModelMapperConfig.java
@@ -4,11 +4,19 @@ import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.oliveira.oliveirafood.api.model.RestauranteModel;
+import com.oliveira.oliveirafood.domain.model.Restaurante;
+
 @Configuration
 public class ModelMapperConfig {
 	
 	@Bean
 	public ModelMapper modelMapper() {
-		return new ModelMapper();	
+		var modelMapper = new ModelMapper();
+		
+		modelMapper.createTypeMap(Restaurante.class, RestauranteModel.class)
+			.addMapping(Restaurante::getTaxaFrete, RestauranteModel::setPrecoFrete);
+		
+		return modelMapper;
 	}
 }
