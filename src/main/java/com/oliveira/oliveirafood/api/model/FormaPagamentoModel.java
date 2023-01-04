8b968e674c69a7e64edf9d3314554d5d7183671c
alissonos <alissonoliveirasilva.oliveira@gmail.com>
2023-01-03 23:07:19 -0300

    implementando os endpoints de formas de pagamento

---
+++ src/main/java/com/oliveira/oliveirafood/api/model/FormaPagamentoModel.java
@@ -0,0 +1,13 @@
+package com.oliveira.oliveirafood.api.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class FormaPagamentoModel {
+
+    private Long id;
+    private String descricao;
+    
+}
