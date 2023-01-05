69d61fbad904195871f7d1e459b6b7f506b503b2
alissonos <alissonoliveirasilva.oliveira@gmail.com>
2023-01-04 22:02:28 -0300

    Adicionando endereço no modelo da representação do recurso de restaurante

--- src/main/java/com/oliveira/oliveirafood/core/modelmapper/ModelMapperConfig.java
+++ src/main/java/com/oliveira/oliveirafood/core/modelmapper/ModelMapperConfig.java
@@ -4,8 +4,10 @@ import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import com.oliveira.oliveirafood.api.model.RestauranteModel;
-import com.oliveira.oliveirafood.domain.model.Restaurante;
+import com.oliveira.oliveirafood.api.model.EnderecoModel;
+import com.oliveira.oliveirafood.domain.model.Endereco;
+
+import lombok.experimental.var;
 
 @Configuration
 public class ModelMapperConfig {
@@ -14,8 +16,15 @@ public class ModelMapperConfig {
 	public ModelMapper modelMapper() {
 		var modelMapper = new ModelMapper();
 		
-		modelMapper.createTypeMap(Restaurante.class, RestauranteModel.class)
-			.addMapping(Restaurante::getTaxaFrete, RestauranteModel::setPrecoFrete);
+//		modelMapper.createTypeMap(Restaurante.class, RestauranteModel.class)
+//			.addMapping(Restaurante::getTaxaFrete, RestauranteModel::setPrecoFrete);
+		
+		var enderecoToEnderecoModelTypeMap = modelMapper.createTypeMap
+				(Endereco.class, EnderecoModel.class);
+		
+		enderecoToEnderecoModelTypeMap.<String>addMapping(
+				enderecoSrc -> enderecoSrc.getCidade().getEstado().getNome(),
+				(endereoModelDest, value) -> endereoModelDest.getCidade().setEstado(value));
 		
 		return modelMapper;
 	}
