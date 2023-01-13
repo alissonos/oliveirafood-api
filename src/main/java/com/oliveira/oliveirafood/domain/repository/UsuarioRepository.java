2de120a4dcfa67bb5d90134d4c034bbb83de8a70
alissonos <alissonoliveirasilva.oliveira@gmail.com>
2023-01-12 22:41:53 -0300

    Implementando regra de negócio para evitar usuários com e-mails duplicados

--- src/main/java/com/oliveira/oliveirafood/domain/repository/UsuarioRepository.java
+++ src/main/java/com/oliveira/oliveirafood/domain/repository/UsuarioRepository.java
@@ -1,10 +1,13 @@
 package com.oliveira.oliveirafood.domain.repository;
 
+import java.util.Optional;
+
 import org.springframework.stereotype.Repository;
 
 import com.oliveira.oliveirafood.domain.model.Usuario;
 
 @Repository
 public interface UsuarioRepository extends CustomJpaRepository<Usuario, Long> {
-
+	
+	Optional<Usuario> findByEmail(String email);
 }
