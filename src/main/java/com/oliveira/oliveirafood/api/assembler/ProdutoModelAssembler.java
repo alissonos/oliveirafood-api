4255f21f3df0f3f1b20628f42c68beaa1ee2eca0
alissonos <alissonoliveirasilva.oliveira@gmail.com>
2023-01-18 22:08:31 -0300

    implementando os endpoints de produtos

---
+++ src/main/java/com/oliveira/oliveirafood/api/assembler/ProdutoModelAssembler.java
@@ -0,0 +1,28 @@
+package com.oliveira.oliveirafood.api.assembler;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.oliveira.oliveirafood.api.model.ProdutoModel;
+import com.oliveira.oliveirafood.domain.model.Produto;
+
+@Component
+public class ProdutoModelAssembler {
+
+    @Autowired
+    private ModelMapper modelMapper;
+    
+    public ProdutoModel toModel(Produto produto) {
+        return modelMapper.map(produto, ProdutoModel.class);
+    }
+    
+    public List<ProdutoModel> toCollectionModel(List<Produto> produtos) {
+        return produtos.stream()
+                .map(produto -> toModel(produto))
+                .collect(Collectors.toList());
+    }   
+}
---
+++ src/main/java/com/oliveira/oliveirafood/domain/service/CadastroProdutoService.java
@@ -0,0 +1,26 @@
+package com.oliveira.oliveirafood.domain.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.oliveira.oliveirafood.domain.exception.ProdutoNaoEncontradoException;
+import com.oliveira.oliveirafood.domain.model.Produto;
+import com.oliveira.oliveirafood.domain.repository.ProdutoRepository;
+
+@Service
+public class CadastroProdutoService {
+
+    @Autowired
+    private ProdutoRepository produtoRepository;
+    
+    @Transactional
+    public Produto salvar(Produto produto) {
+        return produtoRepository.save(produto);
+    }
+    
+    public Produto buscarOuFalhar(Long restauranteId, Long produtoId) {
+        return produtoRepository.findById(restauranteId, produtoId)
+            .orElseThrow(() -> new ProdutoNaoEncontradoException(restauranteId, produtoId));
+    }   
+}
