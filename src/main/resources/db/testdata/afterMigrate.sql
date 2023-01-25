91a8b4c9a7eb63369535965f55c380b924a3ef21
alissonos <alissonoliveirasilva.oliveira@gmail.com>
2023-01-24 21:53:36 -0300

    implementando os endpoints de associação de usuários com grupos

--- src/main/resources/db/testdata/afterMigrate.sql
+++ src/main/resources/db/testdata/afterMigrate.sql
@@ -81,6 +81,9 @@ insert into usuario (id, nome, email, senha, data_cadastro) values
 (3, 'José Souza', 'jose.aux@oliveirafood.com', '123', utc_timestamp),
 (4, 'Sebastião Martins', 'sebastiao.cad@oliveirafood.com', '123', utc_timestamp);
 
+insert into usuario_grupo (usuario_id, grupo_id) values (1, 1), (1, 2), (2, 2);
+
+
 
 
 
