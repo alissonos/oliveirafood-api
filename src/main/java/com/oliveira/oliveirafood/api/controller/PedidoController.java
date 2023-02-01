b9f91b316c90e89ae0dee081eb6430f1778da1ae
alissonos <alissonoliveirasilva.oliveira@gmail.com>
2023-01-31 23:30:13 -0300

    Otimizando a query de pedidos e retornando model resumido na listagem

--- src/main/java/com/oliveira/oliveirafood/api/controller/PedidoController.java
+++ src/main/java/com/oliveira/oliveirafood/api/controller/PedidoController.java
@@ -9,7 +9,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.oliveira.oliveirafood.api.assembler.PedidoModelAssembler;
+import com.oliveira.oliveirafood.api.assembler.PedidoResumoModelAssembler;
 import com.oliveira.oliveirafood.api.model.PedidoModel;
+import com.oliveira.oliveirafood.api.model.PedidoResumoModel;
 import com.oliveira.oliveirafood.domain.model.Pedido;
 import com.oliveira.oliveirafood.domain.repository.PedidoRepository;
 import com.oliveira.oliveirafood.domain.service.EmissaoPedidoService;
@@ -27,11 +29,14 @@ public class PedidoController {
     @Autowired
     private PedidoModelAssembler pedidoModelAssembler;
     
+    @Autowired
+    private PedidoResumoModelAssembler pedidoResumoModelAssembler;
+    
     @GetMapping
-    public List<PedidoModel> listar() {
+    public List<PedidoResumoModel> listar() {
         List<Pedido> todosPedidos = pedidoRepository.findAll();
         
-        return pedidoModelAssembler.toCollectionModel(todosPedidos);
+        return pedidoResumoModelAssembler.toCollectionModel(todosPedidos);
     }
     
     @GetMapping("/{pedidoId}")
