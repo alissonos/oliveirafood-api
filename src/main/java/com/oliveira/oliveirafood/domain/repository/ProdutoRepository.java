31bad4fcbba896795f084636efa8c4f6851fb88b
alissonos <alissonoliveirasilva.oliveira@gmail.com>
2023-02-24 22:17:29 -0300

    Implementando pesquisas simples na API

--- src/main/java/com/oliveira/oliveirafood/domain/repository/ProdutoRepository.java
+++ src/main/java/com/oliveira/oliveirafood/domain/repository/ProdutoRepository.java
@@ -14,9 +14,13 @@ import com.oliveira.oliveirafood.domain.model.Restaurante;
 @Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Long> {
 
-    @Query("from Produto where restaurante.id = :restaurante and id = :produto")
-    Optional<Produto> findById(@Param("restaurante") Long restauranteId, 
-            @Param("produto") Long produtoId);
-    
-    List<Produto> findByRestaurante(Restaurante restaurante);
-}  
+	@Query("from Produto where restaurante.id = :restaurante and id = :produto")
+	Optional<Produto> findById(@Param("restaurante") Long restauranteId, 
+			@Param("produto") Long produtoId);
+	
+	List<Produto> findTodosByRestaurante(Restaurante restaurante);
+	
+	@Query("from Produto p where p.ativo = true and p.restaurante = :restaurante")
+	List<Produto> findAtivosByRestaurante(Restaurante restaurante);
+	
+} 
